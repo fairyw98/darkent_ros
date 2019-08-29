67e446b2628f773dcd3957726215789fd07a89ad
fearywang <45323308+fearywang@users.noreply.github.com>
2019-08-29 18:21:54 +0800

    Update sub1.cpp

--- sub1.cpp
+++ sub1.cpp
@@ -1,14 +1,16 @@
 #include <ros/ros.h>
-#include "std_msgs/String.h"
+#include <std_msgs/String.h>
 #include <darknet_ros_msgs/BoundingBoxes.h>
 #include <darknet_ros_msgs/BoundingBox.h>
+
+//当订阅与图像有关的节点时，需要引用与opencv有关的库
 #include <opencv2/highgui/highgui.hpp>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
-void chatterCallback(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg)
+void Callback(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg)
 {
     cout<<"==========start==========="<<endl;
     for(int i=0;i<msg->bounding_boxes.size();i++){
@@ -19,16 +21,15 @@ void chatterCallback(const darknet_ros_msgs::BoundingBoxes::ConstPtr& msg)
     cout<<"Bouding Boxes (xmax):" << msg->bounding_boxes[i].xmax <<endl;
     cout<<"Bouding Boxes (ymin):" << msg->bounding_boxes[i].ymin <<endl;
     cout<<"Bouding Boxes (ymax):" << msg->bounding_boxes[i].ymax <<endl;
-    cout << "\033[2J\033[1;1H";     // clear terminal
     }
     cout<<"==========end==========="<<endl;
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "test");
+  ros::init(argc, argv, "sub1");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("/darknet_ros/bounding_boxes", 2000, chatterCallback);
+  ros::Subscriber sub = nh.subscribe("/darknet_ros/bounding_boxes", 100, Callback);
   ros::spin();
   return 0;
 }
